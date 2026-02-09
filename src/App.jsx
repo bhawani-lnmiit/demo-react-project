860a2e6469eb9a55c319b198af477345a2bf19b7
Bhawani Sharma <133356991+bhawani-lnmiit@users.noreply.github.com>
2026-02-09 21:12:37 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Your one-stop destination for beautiful plants and gardening supplies!</p>
+      <button className="get-started-button">Get Started</button>
+    </div>
+  );
+};
+
+export default App;
